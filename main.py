6b78c448df429d0c47e9cd0d2ecd60b04cc48870
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2021-01-03 19:33:22 +0300

    2.0

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import keyPressModule as kp
 from djitellopy import tello
+import numpy as np
+import math
 import time
 import cv2
+
+fSpeed = 117 / 10
+aSpeed = 360 / 10
+interval = 0.25
+
+dInterval = fSpeed * interval
+aInterval = aSpeed * interval
+
+x, y = 500, 500
+a = 0
+yaw = 0
+points = [(0,0), (0,0)]
 
 kp.init()
 me = tello.Tello()
@@ -9,35 +23,55 @@ me.connect()
 print(me.get_battery())
 global img
 
+
 def getKeyboardInput():
+    global x, y, a, yaw
+
     lr, fb, ud, yv = 0, 0, 0, 0
 
     # Declare the speed here
-    speed = 75
+    speed = 15
+    aspeed = 50
+    d = 0
 
     # Left and right control
     if kp.getKey("LEFT"):
         lr = -speed
+        d = dInterval
+        a = -180
+
     elif kp.getKey("RIGHT"):
         lr = speed
+        d = -dInterval
+        a = 180
 
     # Forward and backward control
     if kp.getKey("UP"):
         fb = speed
+        d = dInterval
+        a = 270
+
     elif kp.getKey("DOWN"):
         fb = -speed
+        d = -dInterval
+        a = -90
 
     # Up and Down control
     if kp.getKey("w"):
         ud = speed
+
+
     elif kp.getKey("s"):
         ud = -speed
 
     # Left and right rotate control
     if kp.getKey("a"):
-        yv = -speed
+        yv = -aspeed
+        yaw -= aInterval
+
     elif kp.getKey("d"):
-        yv = speed
+        yv = aspeed
+        yaw += aInterval
 
     # Taking off
     if kp.getKey("t"):
@@ -54,7 +88,22 @@ def getKeyboardInput():
         cv2.imwrite(f"Media/{time.time()}.jpg", img)
         time.sleep(0.35)
 
-    return [lr, fb, ud, yv]
+    time.sleep(0.25)
+    a += yaw
+    x += int(d * math.cos(math.radians(a)))
+    y += int(d * math.sin(math.radians(a)))
+
+    return [lr, fb, ud, yv, x, y]
+
+
+def drawPoints(img, points):
+    for point in points:
+        cv2.circle(img, point, 5, (0, 0, 255), cv2.FILLED)
+
+    cv2.circle(img, points[-1], 8, (0, 255, 0), cv2.FILLED)
+    cv2.putText(img, f'({(points[-1][0] - 500) / 100}, {(points[-1][1] - 500) / 100}M',
+                (points[-1][0] + 10, points[-1][1] + 30), cv2.FONT_HERSHEY_PLAIN, 1,
+                (255, 0, 255), 1)
 
 
 me.streamon()
@@ -63,7 +112,11 @@ while True:
     values = getKeyboardInput()
     me.send_rc_control(values[0], values[1], values[2], values[3])
 
-    img = me.get_frame_read().frame
-    img = cv2.resize(img, (360,240))
-    cv2.imshow("Image", img)
+    img = np.zeros((1000,1000,3), np.uint8)
+
+    if (points[-1][0] != values[4] or points[-1][1] != values[5]):
+        points.append((values[4], values[5]))
+
+    drawPoints(img, points)
+    cv2.imshow("Output", img)
     cv2.waitKey(1)
